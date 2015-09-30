a1e2f7a90343c068b82a20bef6c8f8457b392837
Tommy <tmcleroy@gmail.com>
2015-09-29 23:04:50 -0500

    many changes to player movement

--- Assets/Scripts/Controllers/BipedalPlayer.cs
+++ Assets/Scripts/Controllers/BipedalPlayer.cs
@@ -16,13 +16,22 @@ public class BipedalPlayer : MonoBehaviour {
   public float legUpForce;
   public float calfMass;
   public float airTimeBeforeFall;
-  private List<float> springVals = new List<float>();
-  private float secondsInAir;
 
-  // movement vectors
+	// tracks how long both feet have been in the air
+	private float secondsInAir;
+  // bone movement vectors
   private Dictionary<string, Vector3> mVecs = new Dictionary<string, Vector3>();
-  // bone rigidbodies
+  // bone rigid bodies
   private Dictionary<string, Rigidbody> rigidBodies = new Dictionary<string, Rigidbody>();
+	// bone capsule colliders
+	private Dictionary<string, CapsuleCollider> capsuleColliders = new Dictionary<string, CapsuleCollider>();
+	// preserves the original capsule collider height so they can be restored
+  private float capsuleColliderHeight;
+	// preserves the original spring values so springs can be reinitialized
+  private List<float> springVals = new List<float>();
+	// velocities of the legs
+	private float legLeftVel;
+	private float legRightVel;
 
   void Start () {
     rigidBodies["spine"] = spine.GetComponent<Rigidbody>();
@@ -30,59 +39,69 @@ public class BipedalPlayer : MonoBehaviour {
     rigidBodies["thighRight"] = thighRight.GetComponent<Rigidbody>();
     rigidBodies["calfLeft"] = calfLeft.GetComponent<Rigidbody>();
     rigidBodies["calfRight"] = calfRight.GetComponent<Rigidbody>();
+
+		capsuleColliders["calfLeft"] = calfLeft.GetComponent<CapsuleCollider>();
+		capsuleColliders["calfRight"] = calfRight.GetComponent<CapsuleCollider>();
+		capsuleColliderHeight = capsuleColliders["calfLeft"].height;
   }
 
   void FixedUpdate () {
     updateMovementVectors();
     updateMovementConstraints();
+		handleSpecialInput();
   }
 
 
   private void updateMovementVectors () {
-    float legLeftVel = Input.GetKey("a") ? -1 : 0;
-    float legRightVel = Input.GetKey("d") ? -1 : 0;;
+    legLeftVel = Input.GetKey("a") ? -1 : 0;
+    legRightVel = Input.GetKey("d") ? -1 : 0;;
 
     mVecs["legLeft"] = new Vector3(legUpForce, legLeftVel, 0.0f);
     mVecs["legRight"] = new Vector3(legUpForce, legRightVel, 0.0f);
     mVecs["calfLeftRotate"] = new Vector3(0, 0, legLeftVel != 0 ? kneeBend : 0);
     mVecs["calfRightRotate"] = new Vector3(0, 0, legRightVel != 0 ? kneeBend : 0);
 
-    if (legLeftVel != 0 || legRightVel != 0) {
-      moveLegsForward();
-    }
-
-    // remove drag from the moving leg so it can move freely
-    rigidBodies["calfLeft"].drag = legLeftVel == 0 ? legDrag : 0;
-    rigidBodies["calfRight"].drag = legRightVel == 0 ? legDrag : 0;
-
-    if (Input.GetKey("p")) {
-      disableSkeletonSprings();
-    }
-    if (Input.GetKey("l")) {
-      enableSkeletonSprings();
-    }
+    moveLegs();
   }
 
   private void updateMovementConstraints () {
+		// at least one calf is grounded
     if (calfLeft.isCollidingWithFloor || calfRight.isCollidingWithFloor) {
       secondsInAir = 0;
     } else {
       secondsInAir += Time.deltaTime;
-      Debug.Log("both in air " + secondsInAir);
+      // Debug.Log("both in air for " + secondsInAir);
     }
     if (secondsInAir > airTimeBeforeFall) {
       disableSkeletonSprings();
     }
   }
 
-  private void moveLegsForward () {
-    rigidBodies["thighLeft"].AddRelativeForce(mVecs["legLeft"] * (speed + 10));
-    rigidBodies["thighRight"].AddRelativeForce(mVecs["legRight"] * (speed + 10));
-    rigidBodies["calfLeft"].AddRelativeForce(mVecs["legLeft"] * (speed - 10));
-    rigidBodies["calfRight"].AddRelativeForce(mVecs["legRight"] * (speed - 10));
+	private void handleSpecialInput () {
+		if (Input.GetKey("p")) {
+      disableSkeletonSprings();
+    }
+    if (Input.GetKey("l")) {
+      enableSkeletonSprings();
+    }
+	}
 
-    calfLeft.transform.Rotate(mVecs["calfLeftRotate"]);
-    calfRight.transform.Rotate(mVecs["calfRightRotate"]);
+  private void moveLegs () {
+    rigidBodies["thighLeft"].AddRelativeForce(mVecs["legLeft"] * (speed + 10) * Time.deltaTime);
+    rigidBodies["thighRight"].AddRelativeForce(mVecs["legRight"] * (speed + 10) * Time.deltaTime);
+    rigidBodies["calfLeft"].AddRelativeForce(mVecs["legLeft"] * (speed - 10) * Time.deltaTime);
+    rigidBodies["calfRight"].AddRelativeForce(mVecs["legRight"] * (speed - 10) * Time.deltaTime);
+
+    calfLeft.transform.Rotate(mVecs["calfLeftRotate"] * Time.deltaTime);
+    calfRight.transform.Rotate(mVecs["calfRightRotate"] * Time.deltaTime);
+
+		// remove drag from the moving leg so it can move freely
+    rigidBodies["calfLeft"].drag = legLeftVel == 0 ? legDrag : 0;
+    rigidBodies["calfRight"].drag = legRightVel == 0 ? legDrag : 0;
+
+		// adjust the height of the calf capsule colliders so they don't hit the ground as easily
+		capsuleColliders["calfLeft"].height = capsuleColliderHeight * (legLeftVel == 0 ? 1 : 0.7f);
+    capsuleColliders["calfRight"].height = capsuleColliderHeight * (legRightVel == 0 ? 1 : 0.7f);
   }
 
   private void disableSkeletonSprings () {
--- Assets/Scripts/Controllers/QuadripedalPlayerController.cs
+++ Assets/Scripts/Controllers/QuadripedalPlayerController.cs
@@ -4,88 +4,88 @@ using System.Collections.Generic;
 
 public class QuadripedalPlayerController : MonoBehaviour {
 
-	public GameObject spine;
+  public GameObject spine;
 
-	public GameObject thighRearLeft;
-	public GameObject thighRearRight;
-	public GameObject calfRearLeft;
-	public GameObject calfRearRight;
+  public GameObject thighRearLeft;
+  public GameObject thighRearRight;
+  public GameObject calfRearLeft;
+  public GameObject calfRearRight;
 
-	public GameObject thighFrontLeft;
-	public GameObject thighFrontRight;
-	public GameObject calfFrontLeft;
-	public GameObject calfFrontRight;
+  public GameObject thighFrontLeft;
+  public GameObject thighFrontRight;
+  public GameObject calfFrontLeft;
+  public GameObject calfFrontRight;
 
-	public float speed;
-	public float legDrag;
-	public float kneeBend;
+  public float speed;
+  public float legDrag;
+  public float kneeBend;
 
-	// movement vectors
-	private Dictionary<string, Vector3> mVecs = new Dictionary<string, Vector3>();
-	// bone rigidbodies
-	private Dictionary<string, Rigidbody> rigidBodies = new Dictionary<string, Rigidbody>();
+  // movement vectors
+  private Dictionary<string, Vector3> mVecs = new Dictionary<string, Vector3>();
+  // bone rigidbodies
+  private Dictionary<string, Rigidbody> rigidBodies = new Dictionary<string, Rigidbody>();
 
-	void Start () {
-		rigidBodies["spine"] = spine.GetComponent<Rigidbody>();
-		rigidBodies["thighFrontLeft"] = thighFrontLeft.GetComponent<Rigidbody>();
-		rigidBodies["thighFrontRight"] = thighFrontRight.GetComponent<Rigidbody>();
-		rigidBodies["calfFrontLeft"] = calfFrontLeft.GetComponent<Rigidbody>();
-		rigidBodies["calfFrontRight"] = calfFrontRight.GetComponent<Rigidbody>();
-		rigidBodies["thighRearLeft"] = thighRearLeft.GetComponent<Rigidbody>();
-		rigidBodies["thighRearRight"] = thighRearRight.GetComponent<Rigidbody>();
-		rigidBodies["calfRearLeft"] = calfRearLeft.GetComponent<Rigidbody>();
-		rigidBodies["calfRearRight"] = calfRearRight.GetComponent<Rigidbody>();
-	}
+  void Start () {
+    rigidBodies["spine"] = spine.GetComponent<Rigidbody>();
+    rigidBodies["thighFrontLeft"] = thighFrontLeft.GetComponent<Rigidbody>();
+    rigidBodies["thighFrontRight"] = thighFrontRight.GetComponent<Rigidbody>();
+    rigidBodies["calfFrontLeft"] = calfFrontLeft.GetComponent<Rigidbody>();
+    rigidBodies["calfFrontRight"] = calfFrontRight.GetComponent<Rigidbody>();
+    rigidBodies["thighRearLeft"] = thighRearLeft.GetComponent<Rigidbody>();
+    rigidBodies["thighRearRight"] = thighRearRight.GetComponent<Rigidbody>();
+    rigidBodies["calfRearLeft"] = calfRearLeft.GetComponent<Rigidbody>();
+    rigidBodies["calfRearRight"] = calfRearRight.GetComponent<Rigidbody>();
+  }
 
-	void FixedUpdate () {
-		updateMovementVectors();
-	}
+  void FixedUpdate () {
+    updateMovementVectors();
+  }
 
 
-	private void updateMovementVectors () {
-		float legFrontLeftVel = Input.GetKey("q") ? -1 : 0;
-		float legFrontRightVel = Input.GetKey("e") ? -1 : 0;
-		float legRearLeftVel = Input.GetKey("a") ? -1 : 0;
-		float legRearRightVel = Input.GetKey("d") ? -1 : 0;
+  private void updateMovementVectors () {
+    float legFrontLeftVel = Input.GetKey("q") ? -1 : 0;
+    float legFrontRightVel = Input.GetKey("e") ? -1 : 0;
+    float legRearLeftVel = Input.GetKey("a") ? -1 : 0;
+    float legRearRightVel = Input.GetKey("d") ? -1 : 0;
 
-		mVecs["legFrontLeft"] = new Vector3(0.0f, legFrontLeftVel, 0.0f);
-		mVecs["legFrontRight"] = new Vector3(0.0f, legFrontRightVel, 0.0f);
-		mVecs["calfFrontLeftRotate"] = new Vector3(0, 0, legFrontLeftVel != 0 ? -kneeBend : 0);
-		mVecs["calfFrontRightRotate"] = new Vector3(0, 0, legFrontRightVel != 0 ? -kneeBend : 0);
-		mVecs["legRearLeft"] = new Vector3(0.0f, legRearLeftVel, 0.0f);
-		mVecs["legRearRight"] = new Vector3(0.0f, legRearRightVel, 0.0f);
-		mVecs["calfRearLeftRotate"] = new Vector3(0, 0, legRearLeftVel != 0 ? kneeBend : 0);
-		mVecs["calfRearRightRotate"] = new Vector3(0, 0, legRearRightVel != 0 ? kneeBend : 0);
+    mVecs["legFrontLeft"] = new Vector3(0.0f, legFrontLeftVel, 0.0f);
+    mVecs["legFrontRight"] = new Vector3(0.0f, legFrontRightVel, 0.0f);
+    mVecs["calfFrontLeftRotate"] = new Vector3(0, 0, legFrontLeftVel != 0 ? -kneeBend : 0);
+    mVecs["calfFrontRightRotate"] = new Vector3(0, 0, legFrontRightVel != 0 ? -kneeBend : 0);
+    mVecs["legRearLeft"] = new Vector3(0.0f, legRearLeftVel, 0.0f);
+    mVecs["legRearRight"] = new Vector3(0.0f, legRearRightVel, 0.0f);
+    mVecs["calfRearLeftRotate"] = new Vector3(0, 0, legRearLeftVel != 0 ? kneeBend : 0);
+    mVecs["calfRearRightRotate"] = new Vector3(0, 0, legRearRightVel != 0 ? kneeBend : 0);
 
-		if (legFrontLeftVel != 0 || legFrontRightVel != 0 || legRearLeftVel != 0 || legRearRightVel != 0) {
-			moveLegsForward();
-		}
+    if (legFrontLeftVel != 0 || legFrontRightVel != 0 || legRearLeftVel != 0 || legRearRightVel != 0) {
+      moveLegsForward();
+    }
 
-		// remove drag from the moving leg so it can move freely
-		rigidBodies["calfFrontLeft"].drag = legFrontLeftVel == 0 ? legDrag : 0;
-		rigidBodies["calfFrontRight"].drag = legFrontRightVel == 0 ? legDrag : 0;
-		rigidBodies["calfRearLeft"].drag = legRearLeftVel == 0 ? legDrag : 0;
-		rigidBodies["calfRearRight"].drag = legRearRightVel == 0 ? legDrag : 0;
-	}
+    // remove drag from the moving leg so it can move freely
+    rigidBodies["calfFrontLeft"].drag = legFrontLeftVel == 0 ? legDrag : 0;
+    rigidBodies["calfFrontRight"].drag = legFrontRightVel == 0 ? legDrag : 0;
+    rigidBodies["calfRearLeft"].drag = legRearLeftVel == 0 ? legDrag : 0;
+    rigidBodies["calfRearRight"].drag = legRearRightVel == 0 ? legDrag : 0;
+  }
 
-	private void moveLegsForward () {
-		rigidBodies["thighFrontLeft"].AddRelativeForce(mVecs["legFrontLeft"] * (speed + 10));
-		rigidBodies["thighFrontRight"].AddRelativeForce(mVecs["legFrontRight"] * (speed + 10));
-		rigidBodies["calfFrontLeft"].AddRelativeForce(mVecs["legFrontLeft"] * (speed - 10));
-		rigidBodies["calfFrontRight"].AddRelativeForce(mVecs["legFrontRight"] * (speed - 10));
-		rigidBodies["thighRearLeft"].AddRelativeForce(mVecs["legRearLeft"] * (speed + 10));
-		rigidBodies["thighRearRight"].AddRelativeForce(mVecs["legRearRight"] * (speed + 10));
-		rigidBodies["calfRearLeft"].AddRelativeForce(mVecs["legRearLeft"] * (speed - 10));
-		rigidBodies["calfRearRight"].AddRelativeForce(mVecs["legRearRight"] * (speed - 10));
+  private void moveLegsForward () {
+    rigidBodies["thighFrontLeft"].AddRelativeForce(mVecs["legFrontLeft"] * (speed + 10));
+    rigidBodies["thighFrontRight"].AddRelativeForce(mVecs["legFrontRight"] * (speed + 10));
+    rigidBodies["calfFrontLeft"].AddRelativeForce(mVecs["legFrontLeft"] * (speed - 10));
+    rigidBodies["calfFrontRight"].AddRelativeForce(mVecs["legFrontRight"] * (speed - 10));
+    rigidBodies["thighRearLeft"].AddRelativeForce(mVecs["legRearLeft"] * (speed + 10));
+    rigidBodies["thighRearRight"].AddRelativeForce(mVecs["legRearRight"] * (speed + 10));
+    rigidBodies["calfRearLeft"].AddRelativeForce(mVecs["legRearLeft"] * (speed - 10));
+    rigidBodies["calfRearRight"].AddRelativeForce(mVecs["legRearRight"] * (speed - 10));
 
-		calfFrontLeft.transform.Rotate(mVecs["calfFrontLeftRotate"]);
-		calfFrontRight.transform.Rotate(mVecs["calfFrontRightRotate"]);
-		calfRearLeft.transform.Rotate(mVecs["calfRearLeftRotate"]);
-		calfRearRight.transform.Rotate(mVecs["calfRearRightRotate"]);
-	}
+    calfFrontLeft.transform.Rotate(mVecs["calfFrontLeftRotate"]);
+    calfFrontRight.transform.Rotate(mVecs["calfFrontRightRotate"]);
+    calfRearLeft.transform.Rotate(mVecs["calfRearLeftRotate"]);
+    calfRearRight.transform.Rotate(mVecs["calfRearRightRotate"]);
+  }
 
-	// private void moveLegsBackward (Vector3 mVecs["legLeft"], Vector3 mVecs["legRight"]) {
-	// 	rigidBodies["calfLeft"].AddRelativeForce(mVecs["legLeft"] * (speed - 10));
-	// 	rigidBodies["calfRight"].AddRelativeForce(mVecs["legRight"] * (speed - 10));
-	// }
+  // private void moveLegsBackward (Vector3 mVecs["legLeft"], Vector3 mVecs["legRight"]) {
+  //   rigidBodies["calfLeft"].AddRelativeForce(mVecs["legLeft"] * (speed - 10));
+  //   rigidBodies["calfRight"].AddRelativeForce(mVecs["legRight"] * (speed - 10));
+  // }
 }
